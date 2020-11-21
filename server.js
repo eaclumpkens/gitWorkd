3678752ca5863fd8dd6025475968f0b5c4764b69
Vladimir Alexander Syrovatka <nickelbot5.3@gmail.com>
2020-11-21 13:49:21 -0500

    Moved back to res

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ function Main() {
     console.log("Setting up Webservice");
 
     app = express();
-    app.use(express.static("public"));
+    app.use(express.static("res"));
 
     app.use(express.json()); // for parsing application/json
     app.use(express.urlencoded({
@@ -18,17 +18,6 @@ function Main() {
 
     require("./routes/html-routes.js")(app);
     require("./routes/user-management.js")(app);
-
-    app.get("/api/gitRecieved", (req, res) => {
-        var code = req.query.code;
-        if (code) {
-            res.status(200);
-            res.send("GitHub OAuth Code Recieved: " + code);
-        } else {
-            res.status(404);
-            res.send("Code not found");
-        }
-    });
 
     db.sequelize.sync({
         force: true
