d2446573f5fdbdd334f2ad9faf3aff136359bb23
eaclumpkens <eaclumpkens@gmail.com>
2020-11-22 23:30:16 -0500

    added association to Repo table

--- models/swipe.js
+++ models/swipe.js
@@ -1,15 +1,16 @@
 module.exports = function(sequelize, DataTypes) {
+
+    // User-to-User Compatability Score
     var Swipe = sequelize.define("Swipe", {
-        match_percent: DataTypes.INTEGER
+        match_score: DataTypes.INTEGER
     });
 
-
+    // Compatable Repository w/ FK to User
     Swipe.associate = function(models) {
-
-        Swipe.hasOne(models.User, {
+        Swipe.hasOne(models.Repo, {
             onDelete: null
         });
     }
-
+ 
     return Swipe;
 };
