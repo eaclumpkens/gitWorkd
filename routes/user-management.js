d2a9bfcecc583ae4dc2e9fd3448fb4c84bb06678
eaclumpkens <eaclumpkens@gmail.com>
2020-11-24 18:37:12 -0500

    test

--- routes/user-management.js
+++ routes/user-management.js
@@ -14,7 +14,6 @@ const consts = require("../utils/consts");
 module.exports = function(app) {
 
     // Each of the below routes just handles the HTML page that the user gets sent to.
-
     // index route loads view.html
     app.get("/api/gitRecieved", function(req, res) {
         var code = req.query.code;
