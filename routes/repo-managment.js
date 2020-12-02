cc3bd1b39ec570cbb978a181857f0a3b517ddd96
eaclumpkens <eaclumpkens@gmail.com>
2020-12-01 19:18:53 -0500

    repo test

--- routes/repo-managment.js
+++ routes/repo-managment.js
@@ -23,7 +23,7 @@ module.exports = function(app) {
             }).then((loggedUser) => {
 
                 var accessToken = loggedUser.accessToken;
-                console.log(accessToken);
+                console.log(loggedUser);
 
             })
         }
