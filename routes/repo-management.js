0cb0346c154844a9651608141180c0dee809dbb7
eaclumpkens <eaclumpkens@gmail.com>
2020-12-01 22:12:34 -0500

    handlebars repos

--- routes/repo-management.js
+++ routes/repo-management.js
@@ -35,6 +35,25 @@ module.exports = function(app) {
                         console.log(repos.data.length);
                         console.log(repos.data[1]);
 
+                        var handlebarsRepos = [];
+                        for (var i = 0; i < repos.data.length; i ++) {
+
+                            if (repos.data[i].private) {
+                                continue;
+                            }
+
+                            var nRepo = {
+                                id: repos.data[i].id,
+                                name: repos.data[i].name,
+                                description: repos.data[i].description,
+                                url: repos.data[i].html_url
+                            };
+
+                            handlebarsRepos.push(nRepo);
+                        }
+
+                        res.json(handlebarsRepos);
+
                     } else {
                         console.log("Nothin");
                     }
