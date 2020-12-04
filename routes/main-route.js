91d1efa2dca7258480914de50485a1aff5cfc23a
eaclumpkens <eaclumpkens@gmail.com>
2020-12-04 18:36:28 -0500

    wow

--- routes/main-route.js
+++ routes/main-route.js
@@ -56,18 +56,6 @@ module.exports = function(app) {
                         };
                     };
 
-                    // pull language keys 
-                    var keys = Object.keys(otherRepos[0]);
-                    var languages = [];
-
-                    for (var x = 0; x < keys.length; x++) {
-                        if (keys[x] === 'id' || keys[x] === 'title' || keys[x] === 'description' || keys[x] === 'githubId' || keys[x] === 'createdAt' || keys[x] === 'updatedAt' || keys[x] === 'UserId') {
-                            continue;
-                        } else {
-                            languages.push(keys[x]);
-                        }
-                    };
-
                     // iterate through repos
                     for (var a = 0; a < otherRepos.length; a++) {
 
@@ -79,6 +67,18 @@ module.exports = function(app) {
                             languages: function() {
 
                                 var languages = [];
+
+                                // pull language keys 
+                                var keys = Object.keys(otherRepos[a]);
+                                var languages = [];
+
+                                for (var x = 0; x < keys.length; x++) {
+                                    if (keys[x] === 'id' || keys[x] === 'title' || keys[x] === 'description' || keys[x] === 'githubId' || keys[x] === 'createdAt' || keys[x] === 'updatedAt' || keys[x] === 'UserId') {
+                                        continue;
+                                    } else {
+                                        languages.push(keys[x]);
+                                    }
+                                };
 
                                 for (var b = 0; b < languages.length; b++) {
                                     // iterate through repo lang bytes
