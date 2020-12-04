4bc6fa0a4fb98639ffe3c14f7d6defe5b1c32576
Tanner Kirkpatrick <twk4491@gmail.com>
2020-12-04 15:49:39 -0500

    removed console log

--- routes/main-route.js
+++ routes/main-route.js
@@ -30,7 +30,7 @@ module.exports = function(app) {
                         };
                     };
 
-                    console.log(otherRepos);
+                    
 
                     // pull language keys 
                     var keys = Object.keys(otherRepos[0]);
