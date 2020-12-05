5d43aac4b2e72bb8c7a47633f8774365f41af69c
eaclumpkens <eaclumpkens@gmail.com>
2020-12-04 21:59:55 -0500

    i do a fix

--- routes/main-route.js
+++ routes/main-route.js
@@ -73,11 +73,8 @@ module.exports = function(app) {
                         Object.entries(otherRepos[a]).forEach(([key, value]) => {
                             if (value !== null) {
                                 repoData[`${key}`] = `${value}`;  
-                                console.log(otherRepos[a]);
+                                
                             }
-
-
-                            
                         });
 
                         // pull user data
