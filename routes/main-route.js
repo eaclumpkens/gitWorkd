b047e5e5b67840ead7f3fd309d093f3b58b0525a
Vladimir Alexander Syrovatka <nickelbot5.3@gmail.com>
2020-12-05 16:58:15 -0500

    removed build script

--- routes/main-route.js
+++ routes/main-route.js
@@ -147,7 +147,7 @@ module.exports = function(app) {
                                 }
                             });
                         };
-                         
+
                         getData(repoData);
                     };
 
