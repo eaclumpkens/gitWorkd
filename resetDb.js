db118d90f8c7317514b39ffac00f8f71c00e1e9f
eaclumpkens <eaclumpkens@gmail.com>
2020-12-15 19:15:09 -0500

    jscompress

--- resetDb.js
+++ resetDb.js
@@ -1,10 +1,1 @@
-var db = require("./models");
-
-db.sequelize.sync({
-    force: true
-}).then(function() {
-    console.log("Cleared database!!!");
-    db.sequelize.close().then(function() {
-        console.log("Connection to DB Closed");
-    })
-});
+var db=require("./models");db.sequelize.sync({force:!0}).then(function(){console.log("Cleared database!!!"),db.sequelize.close().then(function(){console.log("Connection to DB Closed")})});
--- utils/consts.js
+++ utils/consts.js
@@ -1,18 +1,1 @@
-module.exports = {
-    GITHUB_AUTH_URL: "https://github.com/login/oauth/access_token",
-    GITHUB_USER_URL: "https://api.github.com/user",
-
-    GITHUB_BASE_API: "https://api.github.com",
-
-    GITHUB_REPO_URL: "https://api.github.com/user/repos",
-    GITHUB_LANG_URL: "https://api.github.com/repos",
-
-    GITHUB_REPO_BY_ID: "https://api.github.com/repositories/",
-
-    SIGN_UP_URL: "https://github.com/login/oauth/authorize?scope=user:email&client_id=",
-
-
-
-    //30 min * 60 secs * 1000ms t   o get 30min in ms
-    MAX_LOGIN_TIME: 30 * 60 * 1000
-}
+module.exports={GITHUB_AUTH_URL:"https://github.com/login/oauth/access_token",GITHUB_USER_URL:"https://api.github.com/user",GITHUB_BASE_API:"https://api.github.com",GITHUB_REPO_URL:"https://api.github.com/user/repos",GITHUB_LANG_URL:"https://api.github.com/repos",GITHUB_REPO_BY_ID:"https://api.github.com/repositories/",SIGN_UP_URL:"https://github.com/login/oauth/authorize?scope=user:email&client_id=",MAX_LOGIN_TIME:1800000};
